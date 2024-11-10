ce05167886e904c6edd297e4b8de28d6d296cb7e
mursidx <iammursid@gmail.com>
2024-11-10 09:18:51 +0530

    Updated User Profile

--- routes/user.js
+++ routes/user.js
@@ -26,51 +26,8 @@ router.get("/logout", function (req, res, next) {
   });
 });
 
-//profile router
-router.get('/profile/:userid', userIsLoggedIn, async function (req, res) {
-  try {
-    let user = await userModel.findOne({ _id: req.params.userid });
-    if (!user) {
-      return res.status(404).send('User not found');
-    }
-    res.render('userProfile', { user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server Error');
-  }
-});
 
 
 
-// Edit Profile Route
-router.get('/profile/edit/:userid', userIsLoggedIn, async function (req, res) {
-  try {
-      let user = await userModel.findOne({ _id: req.params.userid });
-      if (!user) {
-          return res.status(404).send('User not found');
-      }
-      res.render('editProfile', { user });
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-  }
-});
-
-
-// Update profile logic (optional, after form submission)
-router.post('/profile/edit/:userid', userIsLoggedIn, async function (req, res) {
-  try {
-      const { name, email, phone, address } = req.body;
-      let user = await userModel.findByIdAndUpdate(
-          req.params.userid,
-          { name, email, phone, address },
-          { new: true } // Return updated user
-      );
-      res.redirect(`/users/profile/${user._id}`);
-  } catch (error) {
-      console.error(error);
-      res.status(500).send('Server Error');
-  }
-});
 
 module.exports = router;

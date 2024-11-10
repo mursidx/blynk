84fb32bfa2bd2e6cafbeaf54eebfebb33ef56e41
mursidx <iammursid@gmail.com>
2024-11-10 07:34:53 +0530

    Added User Profile

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-
+const { validateAdmin, userIsLoggedIn } = require("../middlewares/admin");
 const { userModel, validateUser } = require("../models/user");
+const { func } = require("joi");
 
 router.get("/login", function (req, res) {
   res.render("user_login");
@@ -28,6 +29,49 @@ router.get("/logout", function (req, res, next) {
     res.redirect('/users/login')
     })
   });
+});
+
+//profile router
+router.get('/profile/:userid', userIsLoggedIn, async function (req, res) {
+  try {
+      let user = await userModel.findOne({ _id: req.params.userid });
+      res.render('userProfile', { user });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+  }
+});
+
+
+// Edit Profile Route
+router.get('/profile/edit/:userid', userIsLoggedIn, async function (req, res) {
+  try {
+      let user = await userModel.findOne({ _id: req.params.userid });
+      if (!user) {
+          return res.status(404).send('User not found');
+      }
+      res.render('editProfile', { user });
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+  }
+});
+
+
+// Update profile logic (optional, after form submission)
+router.post('/profile/edit/:userid', userIsLoggedIn, async function (req, res) {
+  try {
+      const { name, email, phone, address } = req.body;
+      let user = await userModel.findByIdAndUpdate(
+          req.params.userid,
+          { name, email, phone, address },
+          { new: true } // Return updated user
+      );
+      res.redirect(`/users/profile/${user._id}`);
+  } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
